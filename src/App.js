e17ee9a81c2c16a10ecc0c8ab47ac28a04047378
Vikas Narwariya <122281547+vikas-narwariya831@users.noreply.github.com>
2024-06-26 19:36:18 +0530

    update3

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import Home from "./component/pages/Home/Home ";
+import Profile from "./component/pages/Profile/Profile";
 
 function App() {
   return (
     <div className="App">
-      <Home/>
+     <Profile/>
     </div>
   );
 }
---
+++ src/component/pages/Profile/Profile.jsx
@@ -0,0 +1,40 @@
+import "./Profile.css";
+import Topbar from '../../topbar/Topbar'
+import Sidebar from '../../sidebar/Sidebar'
+import Feed from '../../Feed/Feed'
+import Rightbar from "../../rightbar/Rightbar";
+
+export default function Profile() {
+  return (
+    <>
+      <Topbar />
+      <div className="profile">
+        <Sidebar />
+        <div className="profileRight">
+          <div className="profileRightTop">
+            <div className="profileCover">
+              <img
+                className="profileCoverImg"
+                src="assets/post/3.jpeg"
+                alt=""
+              />
+              <img
+                className="profileUserImg"
+                src="assets/person/7.jpeg"
+                alt=""
+              />
+            </div>
+            <div className="profileInfo">
+                <h4 className="profileInfoName">Vikas</h4>
+                <span className="profileInfoDesc"></span>
+            </div>
+          </div>
+          <div className="profileRightBottom">
+            <Feed />
+            <Rightbar profile/>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
